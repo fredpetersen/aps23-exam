8291ac607062fc47b614bf2055f96347021b9b02
Frederik Petersen <fred.p@live.dk>
2023-05-01 19:58:18 +0200

    Add submission for testcase 1 and 2

--- lastcall/submissions/accepted/sub1.py
+++ lastcall/submissions/accepted/sub1.py
@@ -1,1 +1,41 @@
-# Some accepted python solution (This would be scoring group 4)
+# Some accepted python solution (This would currently only socres 1 and 2)
+from collections import defaultdict 
+from flow import flow
+
+n = int(input())
+ingredients = defaultdict(int)
+
+for _ in range(n):
+  ing, num = input().split()
+  ingredients[ing] = int(num)
+
+m = int(input())
+drinks = defaultdict(list)
+for _ in range(m):
+  drink, *ings = input().split()
+  drinks[drink] = ings
+
+j = int(input())
+customers = defaultdict(list)
+for _ in range(m):
+  customer, *drinkPrefs = input().split()
+  customers[customer] = drinkPrefs
+
+
+graph = defaultdict(lambda : defaultdict(int))
+
+for (ing, num) in ingredients.items():
+  graph["start"][ing] = num
+
+for drink, ings in drinks.items():
+  for ing in ings:
+    graph[ing][drink] = 100
+
+for cust, prefs in customers.items():
+  for pref in prefs:
+    graph[pref][cust] = 1
+  graph[cust]["end"] = 1
+
+fv,fg,cut = flow(graph, "start", "end")
+print(fv)
+print(fg)
